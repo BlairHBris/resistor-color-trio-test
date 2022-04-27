5728a13210023b0a6cd97243a512969b9a09a8e7
Blair Bristow <blairhbristow@gmail.com>
2022-04-27 11:27:00 -0400

    Add soluition 2

--- resistor-color-trio.test.ts
+++ resistor-color-trio.test.ts
@@ -4,8 +4,8 @@ describe("Resistor Colors", () => {
     it("Black and Brown and Red", () => {
         expect(decodedResistorValue(["black", "brown", "red"])).toBe("100 ohms")
     })
-    it("Black and Brown", () => {
-        expect(decodedResistorValue(["black", "brown", "red"])).toBe("100 ohms")
+    it("Violet and Black and Yellow", () => {
+        expect(decodedResistorValue(["violet", "black", "yellow"])).toBe("700 kiloohms")
     })
 })
 
