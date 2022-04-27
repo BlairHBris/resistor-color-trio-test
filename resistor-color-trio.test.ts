4fb7ab6a0de648ffea9dccc7e5e57919db317d2a
Blair Bristow <blairhbristow@gmail.com>
2022-04-27 11:23:39 -0400

    Add solution

--- resistor-color-trio.test.ts
+++ resistor-color-trio.test.ts
@@ -1,1 +1,11 @@
 import { decodedResistorValue } from "./resistor-color-trio";
+
+describe("Resistor Colors", () => {
+    it("Black and Brown and Red", () => {
+        expect(decodedResistorValue(["black", "brown", "red"])).toBe("100 ohms")
+    })
+    it("Black and Brown", () => {
+        expect(decodedResistorValue(["black", "brown", "red"])).toBe("100 ohms")
+    })
+})
+
